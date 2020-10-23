2795fe048c1ceeffcbdcbc7bd941a904d597d788
Willem Green <willberforce15@gmail.com>
2020-10-23 10:22:26 -0500

    first commit

---
+++ src/store.js
@@ -0,0 +1,11 @@
+const bookmarks = [
+   {
+       id: 1,
+       title: 'google example',
+       url: 'https://www.google.com/',
+       description: 'example description of google',
+       rating: 5
+   }
+]
+
+module.exports = { bookmarks }
